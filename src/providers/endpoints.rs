a0f5d75368dc3958d888cc0e84cf9703ca31ae53
NoctisLight <flush715@outlook.com>
2026-02-21 05:32:06 -0700

    Add - Jito TIP and Endpoints

---
+++ src/providers/endpoints.rs
@@ -0,0 +1,19 @@
+pub const JITO_TIP_ACCOUNTS: &[&str] = &[
+    ("96gYZGLnJYVFmbjzopPSU6QiEV5fGqZNyN9nmNhvrZU5"),
+    ("HFqU5x63VTqvQss8hp11i4wVV8bD44PvwucfZ2bU7gRe"),
+    ("Cw8CFyM9FkoMi7K7Crf6HNQqf4uEMzpKw6QNghXLvLkY"),
+    ("ADaUMid9yfUytqMBgopwjb2DTLSokTSzL1zt6iGPaS49"),
+    ("DfXygSm4jCyNCybVYYK6DwvWqjKee8pbDmJGcLWNDXjh"),
+    ("ADuUkR4vqLUMWXxW9gh6D6L8pMSawimctcNZ5pGwDcEt"),
+    ("DttWaMuVvTiduZRnguLF7jNxTgiMBZ1hyAumKUiL2KRL"),
+    ("3AVi9Tg9Uo68tJfuvoKvqKNWKkC5wPdSSdeBnizKZ6jT"),
+] 
+
+pub const JITO_NY: &str = "https://ny.mainnet.block-engine.jito.wtf";
+pub const JITO_FR: &str = "https://frankfurt.mainnet.block-engine.jito.wtf";
+pub const JITO_AMS: &str = "https://amsterdam.mainnet.block-engine.jito.wtf";
+pub const JITO_LON: &str = "https://london.mainnet.block-engine.jito.wtf";
+pub const JITO_SLC: &str = "https://slc.mainnet.block-engine.jito.wtf";
+pub const JITO_TOKYO: &str = "https://tokyo.mainnet.block-engine.jito.wtf";
+pub const JITO_SGP: &str = "https://singapore.mainnet.block-engine.jito.wtf";
+
