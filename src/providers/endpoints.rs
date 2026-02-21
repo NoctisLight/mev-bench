0d94137ecdd8f24d431d6e57ef33e10f66ff205e
NoctisLight <flush715@outlook.com>
2026-02-21 05:46:52 -0700

    Chore - Jito cleaned, temporal - 0xslot, astralne, stellium, sender tips wallets

--- src/providers/endpoints.rs
+++ src/providers/endpoints.rs
@@ -1,19 +1,76 @@
 pub const JITO_TIP_ACCOUNTS: &[&str] = &[
-    ("96gYZGLnJYVFmbjzopPSU6QiEV5fGqZNyN9nmNhvrZU5"),
-    ("HFqU5x63VTqvQss8hp11i4wVV8bD44PvwucfZ2bU7gRe"),
-    ("Cw8CFyM9FkoMi7K7Crf6HNQqf4uEMzpKw6QNghXLvLkY"),
-    ("ADaUMid9yfUytqMBgopwjb2DTLSokTSzL1zt6iGPaS49"),
-    ("DfXygSm4jCyNCybVYYK6DwvWqjKee8pbDmJGcLWNDXjh"),
-    ("ADuUkR4vqLUMWXxW9gh6D6L8pMSawimctcNZ5pGwDcEt"),
-    ("DttWaMuVvTiduZRnguLF7jNxTgiMBZ1hyAumKUiL2KRL"),
-    ("3AVi9Tg9Uo68tJfuvoKvqKNWKkC5wPdSSdeBnizKZ6jT"),
+    "96gYZGLnJYVFmbjzopPSU6QiEV5fGqZNyN9nmNhvrZU5",
+    "HFqU5x63VTqvQss8hp11i4wVV8bD44PvwucfZ2bU7gRe",
+    "Cw8CFyM9FkoMi7K7Crf6HNQqf4uEMzpKw6QNghXLvLkY",
+    "ADaUMid9yfUytqMBgopwjb2DTLSokTSzL1zt6iGPaS49",
+    "DfXygSm4jCyNCybVYYK6DwvWqjKee8pbDmJGcLWNDXjh",
+    "ADuUkR4vqLUMWXxW9gh6D6L8pMSawimctcNZ5pGwDcEt",
+    "DttWaMuVvTiduZRnguLF7jNxTgiMBZ1hyAumKUiL2KRL",
+    "3AVi9Tg9Uo68tJfuvoKvqKNWKkC5wPdSSdeBnizKZ6jT",
 ] 
 
-pub const JITO_NY: &str = "https://ny.mainnet.block-engine.jito.wtf";
-pub const JITO_FR: &str = "https://frankfurt.mainnet.block-engine.jito.wtf";
-pub const JITO_AMS: &str = "https://amsterdam.mainnet.block-engine.jito.wtf";
-pub const JITO_LON: &str = "https://london.mainnet.block-engine.jito.wtf";
-pub const JITO_SLC: &str = "https://slc.mainnet.block-engine.jito.wtf";
-pub const JITO_TOKYO: &str = "https://tokyo.mainnet.block-engine.jito.wtf";
-pub const JITO_SGP: &str = "https://singapore.mainnet.block-engine.jito.wtf";
+pub const TEMPORAL_TIP_ACCOUNTS: &[Pubkey] = &[
+    "TEMPaMeCRFAS9EKF53Jd6KpHxgL47uWLcpFArU1Fanq"
+    "noz3jAjPiHuBPqiSPkkugaJDkJscPuRhYnSpbi8UvC4"
+    "noz3str9KXfpKknefHji8L1mPgimezaiUyCHYMDv1GE"
+    "noz6uoYCDijhu1V7cutCpwxNiSovEwLdRHPwmgCGDNo"
+    "noz9EPNcT7WH6Sou3sr3GGjHQYVkN3DNirpbvDkv9YJ"
+    "nozc5yT15LazbLTFVZzoNZCwjh3yUtW86LoUyqsBu4L"
+    "nozFrhfnNGoyqwVuwPAW4aaGqempx4PU6g6D9CJMv7Z"
+    "nozievPk7HyK1Rqy1MPJwVQ7qQg2QoJGyP71oeDwbsu"
+    "noznbgwYnBLDHu8wcQVCEw6kDrXkPdKkydGJGNXGvL7"
+    "nozNVWs5N8mgzuD3qigrCG2UoKxZttxzZ85pvAQVrbP"
+    "nozpEGbwx4BcGp6pvEdAh1JoC2CQGZdU6HbNP1v2p6P"
+    "nozrhjhkCr3zXT3BiT4WCodYCUFeQvcdUkM7MqhKqge"
+    "nozrwQtWhEdrA6W8dkbt9gnUaMs52PdAv5byipnadq3"
+    "nozUacTVWub3cL4mJmGCYjKZTnE9RbdY5AP46iQgbPJ"
+    "nozWCyTPppJjRuw2fpzDhhWbW355fzosWSzrrMYB1Qk"
+    "nozWNju6dY353eMkMqURqwQEoM3SFgEKC6psLCSfUne"
+    "nozxNBgWohjR75vdspfxR5H9ceC7XXH99xpxhVGt3Bb"
+];
 
+pub const ZEROSLOT_TIP_ACCOUNTS: &[Pubkey] = &[
+    "6fQaVhYZA4w3MBSXjJ81Vf6W1EDYeUPXpgVQ6UQyU1Av"
+    "4HiwLEP2Bzqj3hM2ENxJuzhcPCdsafwiet3oGkMkuQY4"
+    "7toBU3inhmrARGngC7z6SjyP85HgGMmCTEwGNRAcYnEK"
+    "8mR3wB1nh4D6J9RUCugxUpc6ya8w38LPxZ3ZjcBhgzws"
+    "6SiVU5WEwqfFapRuYCndomztEwDjvS5xgtEof3PLEGm9"
+    "TpdxgNJBWZRL8UXF5mrEsyWxDWx9HQexA9P1eTWQ42p"
+    "D8f3WkQu6dCF33cZxuAsrKHrGsqGP2yvAHf8mX6RXnwf"
+    "GQPFicsy3P3NXxB5piJohoxACqTvWE9fKpLgdsMduoHE"
+    "Ey2JEr8hDkgN8qKJGrLf2yFjRhW7rab99HVxwi5rcvJE"
+    "4iUgjMT8q2hNZnLuhpqZ1QtiV8deFPy2ajvvjEpKKgsS"
+    "3Rz8uD83QsU8wKvZbgWAPvCNDU6Fy8TSZTMcPm3RB6zt"
+];
+
+pub const ASTRALANE_TIP_ACCOUNTS: &[Pubkey] = &[
+    "astrazznxsGUhWShqgNtAdfrzP2G83DzcWVJDxwV9bF"
+    "astra4uejePWneqNaJKuFFA8oonqCE1sqF6b45kDMZm"
+    "astra9xWY93QyfG6yM8zwsKsRodscjQ2uU2HKNL5prk"
+    "astraRVUuTHjpwEVvNBeQEgwYx9w9CFyfxjYoobCZhL"
+    "astraEJ2fEj8Xmy6KLG7B3VfbKfsHXhHrNdCQx7iGJK"
+    "astraubkDw81n4LuutzSQ8uzHCv4BhPVhfvTcYv8SKC"
+    "astraZW5GLFefxNPAatceHhYjfA1ciq9gvfEg2S47xk"
+    "astrawVNP4xDBKT7rAdxrLYiTSTdqtUr63fSMduivXK"
+];
+
+pub const STELLIUM_TIP_ACCOUNTS: &[Pubkey] = &[
+    "ste11JV3MLMM7x7EJUM2sXcJC1H7F4jBLnP9a9PG8PH"
+    "ste11MWPjXCRfQryCshzi86SGhuXjF4Lv6xMXD2AoSt"
+    "ste11p5x8tJ53H1NbNQsRBg1YNRd4GcVpxtDw8PBpmb"
+    "ste11p7e2KLYou5bwtt35H7BM6uMdo4pvioGjJXKFcN"
+    "ste11TMV68LMi1BguM4RQujtbNCZvf1sjsASpqgAvSX"
+];
+
+pub const HELIUS_TIP_ACCOUNTS: &[Pubkey] = &[
+    "4ACfpUFoaSD9bfPdeu6DBt89gB6ENTeHBXCAi87NhDEE"
+    "D2L6yPZ2FmmmTKPgzaMKdhu6EWZcTpLy1Vhx8uvZe7NZ"
+    "9bnz4RShgq1hAnLnZbP8kbgBg1kEmcJBYQq3gQbmnSta"
+    "5VY91ws6B2hMmBFRsXkoAAdsPHBJwRfBht4DXox3xkwn"
+    "2nyhqdwKcJZR2vcqCyrYsaPVdAnFoJjiksCXJ7hfEYgD"
+    "2q5pghRs6arqVjRvT5gfgWfWcHWmw1ZuCzphgd5KfWGJ"
+    "wyvPkWjVZz1M8fHQnMMCDTQDbkManefNNhweYk5WkcF"
+    "3KCKozbAaF75qEU33jtzozcJ29yJuaLJTy2jFdzUY8bT"
+    "4vieeGHPYPG2MmyPRcYjdiDmmhN3ww7hsFNap8pVN3Ey"
+    "4TQLFNWK8AovT1gFvda5jfw2oJeRMKEmw7aH6MGBJ3or"
+];
