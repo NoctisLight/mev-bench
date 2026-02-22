ac13e6029484e9fb65a82619c90fce83012038c0
NoctisLight <flush715@outlook.com>
2026-02-21 23:09:11 -0700

    Deserialized keys from config

--- src/config.rs
+++ src/config.rs
@@ -1,14 +1,24 @@
+use serde::Deserialize;
+
+#[derive(Deserialize)]
 struct Config {
     keys: Keys,
     rpc: Rpc,
 }
 
 // Everything is optinal until i figure how i will make the benchmark [Ping, sending etc]
+#[derive(Deserialize)]
 struct Keys{
     jito_uuid: Option<String>,
     temporal_api_key: Option<String>,
     zeroslot_api_key: Option<String>,
     astralane_api_key: Option<String>,
     stellium_api_key: Option<String>,
-    helius_api_key: Option:<String>
+    helius_api_key: Option<String>
+}
+
+
+#[derive(Deserialize)]
+struct Rpc{
+    url: Option<String>
 }
